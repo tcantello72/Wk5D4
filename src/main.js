bbaa7a3fc722023af00bb361ce68a9a6af8e4aa8
toby.cantello@yum.com <toby.cantello@yum.com>
2023-05-09 08:42:39 -0500

    050923 0841
    update

--- src/main.js
+++ src/main.js
@@ -1,46 +1,32 @@
 const { db } = require("../db/connection.js");
-const Pokemon = require("./model");
+const { Pokemon, Trainer, Badge }  = require("./models");
 
 async function main() {
-  await db.sync({ force: true });
 
-  const Pikachu = await Pokemon.create({
-    name: "Pikachu",
-    type: "Electric",
-    weight: 6,
-  });
-  console.log(JSON.stringify(Pikachu, null, 2));
-  await Pokemon.bulkCreate([
-    {
-      name: "Bulbasaur",
-      type: "Grass/Poison",
-      weight: 6.9,
-    },
-    {
-      name: "Charmander",
-      type: "Fire",
-      weight: 8.5,
-    },
-    {
-      name: "Squirtle",
-      type: "Water",
-      weight: 9,
-    },
-  ]);
+  const trainers = await Trainer.findAll();
+  const pokemon = await Pokemon.findAll();
+  const badges = await Badge.findAll();
 
-  const currentPokemon = await Pokemon.findOne({ where: { name: "Pikachu" } });
-  console.log(JSON.stringify(currentPokemon, null, 2));
-  const allPokemon = await Pokemon.findAll();
-  console.log(JSON.stringify(allPokemon, null, 2));
+  await trainers[0].addPokemon(pokemon[0]);
+  await trainers[0].addPokemon([pokemon[1], pokemon[2]]);
 
-  const updateResult = await Pokemon.update(
-    { name: "Ivysaur", weight: 13 },
-    { where: { name: "Bulbasaur" } }
-  );
-  console.log(updateResult);
+  const trainer = await Trainer.findOne({ where: {name: "Red"} });
+  //console.log(JSON.stringify(trainer, null, 2));
 
-  const deleteResult = await Pokemon.destroy({ where: { name: "Squirtle" } });
-  console.log(deleteResult);
+  const trainerWithPokemon = await Trainer.findOne({ where: {name: "Red"}, include: Pokemon });
+  //console.log(JSON.stringify(trainerWithPokemon, null, 2));
+
+  await trainers[0].addBadge(badges[0]);
+  await trainers[1].addBadge(badges[0]);
+
+  const trainerWithBadges = await Trainer.findAll({include: Badge});
+  //console.log(JSON.stringify(trainerWithBadges, null, 2));
+
+  //magic methods on what you can do with models use npm start
+  //console.log(trainers[0].__proto__)
+  //console.log(pokemon[0].__proto__)
+  //console.log(badges[0].__proto__)
+
 }
 
 main();
--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,12 @@ const Pokemon = require("./Pokemon.js");
 const Trainer = require("./Trainer.js");
 const Badge = require("./Badge.js");
 
+Trainer.hasMany(Pokemon);
+Pokemon.belongsTo(Trainer);
+
+Trainer.belongsToMany(Badge, {through: "TrainerBadges"});
+Badge.belongsToMany(Trainer,  {through: "TrainerBadges"});
+
 module.exports = {
   Pokemon,
   Trainer,
